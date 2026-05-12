a2f8507893a78bc84a65c916c25592941a2cbd8a
Diana <dianadusseaux649@gmail.com>
2026-05-12 17:01:14 -0400

    Subiendo archivo de listas

---
+++ 03-listas.py
@@ -0,0 +1,16 @@
+#LISTAS
+
+#PRIMERA FORMA DE DECLARAR LISTA
+lista1 = ["diana, 22, true"]
+
+#SEGUNDA FORMA DE DECLARAR LISTA
+n = list((1,2,3,4,5))
+
+print(type(n))
+print(type(lista1))
+
+#METODOS PARA LAS LISTAS
+
+#01-COUNT() contar las cantidad de concurrencias de un elemento
+print(lista1.count("diana"))
+
