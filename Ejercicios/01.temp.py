f973d210dda344c376ca896895f25c87d8aeda0d
Diana <dianadusseaux649@gmail.com>
2026-05-26 21:59:24 -0400

    Se realiza ejercicio de temperatura

---
+++ Ejercicios/01.temp.py
@@ -0,0 +1,21 @@
+
+temperatura = list((12.5, 14.2, 11.8))
+
+temp_lunes = temperatura[0]
+
+temp_martes = temperatura[1]
+
+temp_miercoles = temperatura[2]
+
+suma = temp_lunes + temp_martes + temp_miercoles
+
+promedio = suma / 3
+
+temperatura_maxima = max(temperatura)
+temperatura_minima = min(temperatura)
+
+rango_diferencia = temperatura_maxima - temperatura_minima
+#sum sirve para sumar
+#max numero maximo
+#min nu8mero menor
+
