ec77e67c349872494ea4e55744535916db4f3ec1
Diana <dianadusseaux649@gmail.com>
2026-06-02 14:17:00 -0400

    Se actualiza ejercicio 4

--- Guia de ejercicios/04.fisica.py
+++ Guia de ejercicios/04.fisica.py
@@ -1,25 +1,3 @@
-"""4. En f´ısica de part´ıculas, la precisi´on de los decimales es cr´ıtica. Un sensor de presi´on
-hidr´aulica en un laboratorio de la universidad entrega una medida de 1024.7689 Pascales como tipo float. Escribe un programa que realice lo siguiente:"""
-
-#a) Defina la variable con el valor del sensor
-
-
-
-
-"""b) Convierta dicho valor a un n´umero entero (int), descartando los decimales, y
-almac´enelo en una variable nueva."""
-
-
-
-"""c) Utilice un m´etodo propio de Python para redondear el valor original del sensor a
-exactamente 2 decimales y guarde el resultado en otra variable."""
-
-
-
-
-"""d) Imprima un mensaje comparativo que muestre por terminal: el valor original, el
-valor truncado como entero y el valor redondeado."""
-
 
 valor_sensor = 1024.7689
 valor_sensor_entero = int(valor_sensor)
