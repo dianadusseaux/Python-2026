8917d0b178a56a3284b87b2753c6a454dc7cab36
Diana <dianadusseaux649@gmail.com>
2026-06-02 14:16:16 -0400

    Se actualiza ejercicio 1

--- Guia de ejercicios/01.telecomunicaciones.py
+++ Guia de ejercicios/01.telecomunicaciones.py
@@ -1,26 +1,9 @@
-"""En el analisis de antenas y redes de telecomunicaciones, la impedancia de una linea de
-transmision se compone de una parte real (resistencia) y una parte imaginaria (reactancia). Un ingeniero necesita calcular la impedancia total sumando los componentes
-de dos tramos de la red de fibra optica de la universidad."""
-
-
-"""a) Defina la impedancia del Tramo 1 como un numero complejo con parte real 50 y
-parte imaginaria 30 (50 + 30j)."""
-
 impadencia1 = 50 + 30j
 
-"""b) Defina la impedancia del Tramo 2 de la misma forma, con parte real 40 y parte
-imaginaria −10 (40 − 10j).
-"""
 impadencia2 = 40 - 10j
-
-#c) Calcule la impedancia total sumando ambos tramos.
 
 sumaimpadencia = impadencia1 + impadencia2
 
-"""d) Muestre en pantalla la impedancia total, y luego imprima por separado solo la
-parte real (convertida a n´umero entero int) y la parte imaginaria (convertida a
-int) usando los atributos .real y .imag.
-"""
 #parte real
 impadenciareal = int(sumaimpadencia.real)
 
