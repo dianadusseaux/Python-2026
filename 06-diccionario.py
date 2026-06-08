560e58f5e92e0eaf067b6291e0712a0e59826512
Diana <dianadusseaux649@gmail.com>
2026-06-08 13:02:50 -0400

    Se agrega metodos diccionario

---
+++ 06-diccionario.py
@@ -0,0 +1,12 @@
+#DICCIONARIOS
+
+paciente = {
+    "nombre" : "diana dusseaux",
+    "edad" : 22,
+    "ciudad" : "chonchi",
+    "fechas_atencion" : [5,8,12],
+    "diagnostico" : ("resfrio comun")
+}
+
+print(paciente)
+print(f"===FICHA PACIENTE=== \n{paciente}")
