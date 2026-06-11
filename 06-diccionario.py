ad0aeb6f38cc2bd893e00963fa7671a3d7c98b08
Diana <dianadusseaux649@gmail.com>
2026-06-11 16:30:57 -0400

    Se agregan metodos a diccionario

--- 06-diccionario.py
+++ 06-diccionario.py
@@ -1,12 +1,89 @@
 #DICCIONARIOS
 
+# Primera forma de declarar diccionario
 paciente = {
-    "nombre" : "diana dusseaux",
-    "edad" : 22,
-    "ciudad" : "chonchi",
+    "nombre" : "Lorena",
+    "edad" : 29,
+    "ciudad" : "Ancud",
     "fechas_atencion" : [5,8,12],
-    "diagnostico" : ("resfrio comun")
-}
+    "diagnostico" : ("resfrio comun"),
+    "informacion_extra" : {
+        "tipo_de_sangre" : "A+",
+        "hemograma" : False
+    }
+} #Es recomendable usar los diccionarios con espaciado para no confundirlos con sets
 
+# Segunda forma de declarar diccionario
+medico = dict(
+    nombre = "Aaaaaa",
+    edad = 21,
+    especialidad = "Cardiologo"
+)
+
+print(type(paciente))
+print(f"===FICHA PACIENTE=====\n{paciente}\n")
+print(f"===FICHA medico=====\n{medico}\n")
+
+# CONSULTA DE INFORMACION A DICCIONARIOS
+
+# ¿Como consulto el nombre del paciente sin traer el diccionario completo?
+print(f"Nombre del paciente a consultar: {paciente["nombre"]}") # A diferencia de las listas se busca desde el nombre de la clave  
+
+# Metodo get() obtiene el valor de una clave, si no existe retorna None (o un valor por defecto)
+print(paciente.get("Nombre"))
+print(paciente.get("rut", "N/D (No data)"))
+
+# Retornar las claves, los valores o ambas como pares
+print(paciente.keys()) # dict_keys(["nombre","edad",....]) ->solo claves
+print(paciente.values()) # dict_values(["nombre","edad",....]) ->solo valores
+print(paciente.items()) # dict_items(["nombre","edad",....]) ->so
+
+#retorna el numero  de claves que tiene
+print(len(medico))
+print(len(paciente))
+
+# MODIFICACION DEL DICCIONARIO
+# Agregar una clave nueva al diccionario por paciente
+paciente["telefono"] = "+56934568669"
+print(f"=====FICHA PACIENTE=====\n{paciente}\n")
+
+# Sobrescribir valor de una clave
+# Forma 1
+paciente["edad"] = 20
+print("\n=====FICHA PACIENTE CON  EDAD ACTUALIZADA=====\n")
 print(paciente)
-print(f"===FICHA PACIENTE=== \n{paciente}")
+
+# Fusiona otro diccionario (o pares clave-valor) en el actual
+# Util para actualizar varios campos a la vez (actualizar varias claves)
+paciente.update({"edad":21,"ciudad": "Castro"})
+print(paciente["edad"])
+print(paciente["ciudad"])
+print(f"=====FICHA PACIENTE CON  EDAD ACTUALIZADA=====\n{paciente}")
+
+# Eliminar una clave sin retorno
+del(paciente["informacion_extra"])
+print(f"=====FICHA PACIENTE=====\n{paciente}")
+# Eliminar una clave y retornar su valor (a diferencia de del, que no la retorna) -> pop()
+edad_eliminada = paciente.pop("edad")
+print(f"Edad eliminada: {edad_eliminada}") # Se recupera el valor antes de eliminarla
+print(f"=====FICHA PACIENTE=====\n{paciente}")
+
+# OTRAS UTILIDADES DEL DICCIONARIO
+
+# Con in se verifica si una clave existe en el diccionario (sin usar condicionales todavia)
+print("nombre" in paciente)
+print("rut" in paciente)
+
+# Con copy() se crea una copia independiente del diccionario
+paciente2 = paciente.copy()
+paciente2["nombre"] = "BBBB"
+print(paciente["nombre"])
+print(paciente2["nombre"])
+print(paciente2)
+
+# Con clear() elimina todos los elementos del diccionario, dejandolo vacio (aun existe pero esta vacio)
+medico2 = medico.copy()
+print(medico2)
+medico2.clear()
+print(medico2) # ->{}
+
