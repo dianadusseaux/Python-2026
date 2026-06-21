03d189d0122a26289e0d2754f4ac9bdb0affb635
Diana <dianadusseaux649@gmail.com>
2026-06-20 22:26:28 -0400

    Se finaliza ejercicio numero 6 guia evaluada

--- Guía-N°1 Datos estructurados, Bucles y Condicionales/06-reloj_digital.py
+++ Guía-N°1 Datos estructurados, Bucles y Condicionales/06-reloj_digital.py
@@ -6,3 +6,10 @@ for horas in range(24):
     for minutos in range(60):
         for segundos in range(60):
             print(f"{horas:02}:{minutos:02}:{segundos:02}")
+
+
+
+"""
+Javiera ojeda
+Diana Dusseaux
+"""
