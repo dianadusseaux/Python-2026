55e08b129cfa6ea7af8d945221481c46cf10e153
Diana <dianadusseaux649@gmail.com>
2026-06-20 21:41:51 -0400

    Se sube ejercicio 6 guia evaluada

--- Guía-N°1 Datos estructurados, Bucles y Condicionales/06-reloj_digital.py
+++ Guía-N°1 Datos estructurados, Bucles y Condicionales/06-reloj_digital.py
@@ -2,9 +2,7 @@ horas  = 0
 minutos = 0
 segundos = 0
 
-while horas >= 24:
-    horas =+1
-    while minutos >= 60:
-        minutos =+1
-        while segundos >= 60:
-            segundos =+1
+for horas in range(24):
+    for minutos in range(60):
+        for segundos in range(60):
+            print(f"{horas:02}:{minutos:02}:{segundos:02}")
