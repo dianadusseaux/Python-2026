8322b86b52f6d86de11f0844f35582da53335034
Diana <dianadusseaux649@gmail.com>
2026-06-21 18:11:01 -0400

    Apuntes funciones python

---
+++ funciones.py
@@ -0,0 +1,76 @@
+"""
+input("") --> el usuario ingresa por consola
+str --> convierte un objeto en una strings
+max() --> numero maximo
+min() --> numero minimo
+round(3.14159, 3f) --> redondea un numero a una cantidad de decimales
+sum() --> devuelve la suma de los elementos de una lista
+pow() --> eleva un numero a una potencia
+int --> entero
+float --> flotante
+.complex() --> una parte real y una imaginaria
+.lower() --> minuscula
+.upper() -->mayuscula
+.append --> agregar un elemento al final de la lista
+len --> contar las strings en una palabra o elementos dentro de una lista
+strip() --> eliminar los espacios
+split --> DIVIDE LA CADENA ENSUBCADENAS UTILIZANDO UN SEPARADOR ESPECIFICADO
+replace("_", "") --> reemplazar
+.pop() --> eliminar un elemento de la lista
+.sort --> ordena los elementos de una lista de forma descente a ascendente
+.sort(key=len) --> ordena los elementos de la lista dependiendo de la cantidad de caracteres
+.sorted() --> DEVUELVE UNA NUEVA LISTA ORDENADA SIN MODIFICAR LA ORIGINAL
+.remove --> ELIMINA LA PRIMERA OCURRENCIA DE UN ELEMENTO EN LA LISTA
+.insert() --> INSERTA UN ELEMENTO EN UNA POSICIÓN ESPECÍFICA DE LA LISTA
+.extend --> ESTA FUNCIÓN COMBINA DOS O MÁS LISTAS
+.index --> devuelve el indice del elemento seleccionado en la tupla
+.add() --> agregar un nuevo elemento a un sets
+.discard() --> eliminar un elemento de un sets
+.count() --> devuelve el numero de veces que aparece el valor en la tupla
+.intersection() --> DEVUELVE UN NUEVO SET QUE CONTIENE SÓLO LOS ELEMENTOS QUE ESTÁN EN AMBOS SETS
+.difference() --> AL CONTRARIO DE INTERSECTION() DEVUELVE UN NUEVO SET QUE CONTIENE SÓLO LOS ELEMENTOS
+QUE ESTÁN EN EL 1 ° SET PERO NO EN EL 2° SET
+.all() --> devuelve true si todos los elementos de la tupla son verdaderos o si esta vacia
+.any() --> devuelve true si al menos un elemento lo es
+-----DICCIONARIO-----
+del --> borrar elemento de un diccionario al igual que una lista
+.pop() --> accede al elemento indicado y lo elimina al mismo tiempo
+len() --> se cuentan cuantos elementos clave / valor tiene un diccionario
+.get() --> obtiene el valor de una clave de un diccionario
+.keys() --> dict_keys(["nombre","edad",....]) ->solo claves
+.values() --> dict_values(["nombre","edad",....]) ->solo valores
+.item() --> dict_items(["nombre","edad",....]) ->so
+.update() --> fusiona otro diccionario en el actual o es util para actualizar varios campos a la vez
+del({})--> elimina una clave sin retorno
+in --> con in se verifica si una clave existe en un diccionario
+copy() --> se crea una copia independiente de un diccionario
+clear() --> elimina todos los elementos del diccionario dejandolo vacio
+map() --> sirve para reemplazar un for
+.join() --> UNE STRINGS UTILIZANDO LA CADENADE TEXTO QUE LO LLAMA COMO SEPARADOR
+filter() --> sirve para filtrar
+lambda --> es una funcion directa como PI
+zip() --> tomo 2 o mas iterables y las convina en una sola iterable de tupla
+-----BUCLES-----
+if --> si
+elif --> si no se cumple if se cumple elif(elif siempre va despues de if)
+else --> si no se cumple if ni elif procede a cumplirse else
+and --> "y"(se deben cumplir ambas condiciones o las que yo separe con and)
+or --> "o"(solo es necesario que minimamente se cumpla una condicion y se ejecutara)
+not --> "no"(negacion) si una condicion es verdadera la convierte en falsa y si es falsa la convierte en verdadera
+while --> mientras
+for --> se utiliza para iterar una secuencia, se ejecuta hasta que termina de recorrer todos los elementos de la secuencia.
+in --> "en" se utiliza junto al for para saber que elemento se esta iterando
+else dentro de un while --> el else debe estar al nivel del while
+if dentro de un while -->  el if debe estar 4 espacios a diferencia del while
+break --> sirve para quebrar un bucle (solo se puede utilizar en conjunto con el while)
+continue --> sirve para continuar un bucle
+
+
+
+Fore.RED + "texto" --> sirve para colorear el texto (el color debe ir en mayuscula y se utiliza + para contatenar)
+!= --> es lo contrario a ==
+== --> comparación
+= --> asignar
+
+
+"""
