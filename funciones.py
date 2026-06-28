f4350b54014aaa3ce1f541a7832298d3d48653ad
Diana <dianadusseaux649@gmail.com>
2026-06-28 14:14:35 -0400

    Se crea funciones de python y sus significados

--- funciones.py
+++ funciones.py
@@ -72,5 +72,43 @@ Fore.RED + "texto" --> sirve para colorear el texto (el color debe ir en mayuscu
 == --> comparación
 = --> asignar
 
+similar a un segun en pseint
+funciona desde la version 3.9 en adelante
+match --> 
+case-->
+
+print("1. hamburgesa")
+print("2. completo")
+print("3. pizza ")
+
+opcion = input("porfavor , ingresa una opcion (1-3): ")
+match opcion:
+    case "1":
+        print(f"has elegido hamburguesa $")
+    case"2":
+        print(f"has elegido completo $")
+    case"3":
+        print(f"has elegido pizza $")
+    case_:
+        print(f"opcion no valida")
+no se necesita finalizar
+
+mes = 4 #abril
+match mes:
+    case 12 | 1 | 2:
+        print(f"verano")
+    case 3 | 4 |5
+        print(f"otoño")
+    case 6 | 7 | 8
+        print(f"invierno")
+    case 9 | 10 | 11
+        print(f"primavera")
+
+la "|" significa un o es decir si mes es 12 o 1 o 2 entonces escribir verano
+
+hora = 18
+match hora:
+    case h if 0 <- h < 6:
+    
 
 """
