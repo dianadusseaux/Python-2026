5f3556234a448a532c64dd8f7545c420ed5341e1
Diana <dianadusseaux649@gmail.com>
2026-07-02 16:26:37 -0400

    Se finaliza ejercicio 06

--- Guía-N°1 Datos estructurados, Bucles y Condicionales/06-reloj_digital.py
+++ Guía-N°1 Datos estructurados, Bucles y Condicionales/06-reloj_digital.py
@@ -1,3 +1,4 @@
+
 horas  = 0
 minutos = 0
 segundos = 0
@@ -6,6 +7,7 @@ for horas in range(24):
     for minutos in range(60):
         for segundos in range(60):
             print(f"{horas:02}:{minutos:02}:{segundos:02}")
+
 
 
 
