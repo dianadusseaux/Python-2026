e531e6cf3679f8dc799a1eee06f2fa3cbebce2c8
Diana <dianadusseaux649@gmail.com>
2026-07-07 14:13:12 -0400

    Se corrige laboratorio numero 2

--- Laboratorio 2 python/Laboratorio 02.py
+++ Laboratorio 2 python/Laboratorio 02.py
@@ -1,12 +1,12 @@
 #a) crear un diccionario con la variable censo_2017
 censo_2017 = {
-    "14" : {
+    14 : {
         "Nombre Región" : "Los_Rios" ,
         "Superficie" : 18429 ,
         "Habitantes" : 404432 ,
     },
     
-    "12" : {
+    12 : {
         "Nombre Región" : "Magallanes",
         "Superficie" : 1382291,
         "Habitantes" : 166533,
@@ -21,7 +21,6 @@ print(f"El diccionario inicial es: {censo_2017}")
 
 #densidad poblacional = habitantes/superficie
 densidadl = float(round((censo_2017["14"]["Habitantes"])/(censo_2017["14"]["Superficie"]),3))
-
 print(f"La densidad poblacional de la región de Los Rios es de: {densidadl}")
 
 densidadm = float(round((censo_2017["12"]["Habitantes"])/(censo_2017["12"]["Superficie"]),3))
@@ -29,20 +28,20 @@ print(f"La densidad poblacional de la región de Magallanes es de: {densidadm}")
 
 #c) agregar capitales
 
-censo_2017[14] = ["Capital" ":" "Valdivia",]
-censo_2017[14] = ["Comunas" ":" "[Rio Bueno, La Unión, Paillaco]",]
-censo_2017[14] = ["Coordenadas_simuladas" ":" "(23.5 , 85.6)",]
-censo_2017[14] = ["Zonas_exclusivas" ":" "{Urbana , Rural , Fronteriza}",]
+censo_2017[14] = ["Capital"] = "Valdivia"
+censo_2017[14] = ["Comunas"] = [Rio Bueno, La Unión, Paillaco]
+censo_2017[14] = ["Coordenadas_simuladas"] = (23.5 , 85.6)
+censo_2017[14] = ["Zonas_exclusivas"] = {"Urbana" , "Rural" , "Fronteriza"}
 
-censo_2017[12] = ["Capital" ":" "Punta Arenas",]
-censo_2017[12] = ["Comunas" ":" "[Cabo de Hornos, Puerto Williams, Porvenir]",]
-censo_2017[12] = ["Coordenadas_simuladas" ":" "(69.7 , 96.9) ",]
-censo_2017[12] = ["Zonas_exclusivas" ":" "{Urbana , Rural , Fronteriza}",]
+censo_2017[12] = ["Capital"] = "Punta Arenas"
+censo_2017[12] = ["Comunas"] = [Cabo de Hornos, Puerto Williams, Porvenir]
+censo_2017[12] = ["Coordenadas_simuladas"] = (69.7 , 96.9)
+censo_2017[12] = ["Zonas_exclusivas"] = {"Urbana" , "Rural" , "Fronteriza"}
 print(f"{censo_2017}")
 
 
 #d)cambiar el nombre magallanes
-censo_2017["12"]["Nombre Región"] = ["Magallanes y Antártica Chilena"]
+censo_2017[12]["Nombre Región"] = ["Magallanes y Antártica Chilena"]
 
 #e)menu interactivo
 #que solicite al usuario ingresar por teclado el ID 12 o 14 para consultar comunas
