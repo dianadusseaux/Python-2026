42c5b5d07abdecfa89e62e871c7a1046f19beafa
Diana <dianadusseaux649@gmail.com>
2026-07-08 10:36:24 -0400

    Se sube archivo 10 excepciones

--- 10-excepciones.py
+++ 10-excepciones.py
@@ -3,4 +3,11 @@ entrada = input("Ingrese un valor entero: ") #El valor ingresado es un str
 try:
     numero = int(entrada)
     print(f"Número ingresado: {numero}")
-except ValueError
+except ValueError: #error por tipo de dato
+    print(f"Error de valor: el valor ingresado no es número entero")
+except Exception as e: #errores genericos e inesperados
+    print(f"Error inesperado{e}")
+else:
+    print("Conversión fue exitosa!")
+finally:
+    print("Finalización del bloque")
