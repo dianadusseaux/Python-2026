69197dc0267e4e93151239c89a4fd94dacbf5f3b
Diana <dianadusseaux649@gmail.com>
2026-07-09 17:19:57 -0400

    Se actualizan funciones

--- 12-funciones.py
+++ 12-funciones.py
@@ -54,51 +54,6 @@ print(resultado3)
 #caracteristicas(tiene caso base y caso recursivo)
 #los casos recursivos son las divisiones para lograr resolver el caso base"""
 
-print("=====CALCULO FACTORIAL=====")
-#calculo factorial normal
-def factorial_normal(n):
-    r= 1
-    i = 2
-    while i <= n:
-        r *= i
-        i += 1
-    return r
 
-print(factorial_normal(5))
 
-#calculo facotial con recursividad
-def factorial_recursivo(n):
-    if n == 1:
-        return 1
-    else:
-        return n * factorial_recursivo(n-1)#En esta linea se llama a la función dentro de una función
-
-print(factorial_recursivo(5))
-
-#python limita a 1000 llamadas la recursividad
-
-print("=====FIBONACCI=====")
-#Fibonacci metodo recursivo
-def fibonacci_recursivo(n):
-    #casos base(o y 1)
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    #caso recursivo
-    return fibonacci_recursivo(n-1) + fibonacci_recursivo(n-2)
-
-#ejemplo: 5to numero de fibonacci(se empieza desde el 0)
-print(f"5to Número de Fibonacci: {fibonacci_recursivo(5)}")
-
-#Fibonacci metodo iterativo
-def fibonacci_iterativo(n):
-    #casos base (o y 1)
-    a, b = 0, 1
-    for _ in range(n):
-        a, b = b, a + b #Se actualizan los valores
-    return a
-
-#ejemplo: 5to numero de fibonacci(se empieza desde el 0)
-print(f"5to Número de Fibonacci: {fibonacci_iterativo(5)}")
 
